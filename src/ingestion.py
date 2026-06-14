ca72a30a48af6dc5a3fc2dcf808b7e9dc2cd28d0
Murtada Ahmed <murtadhabg@gmail.com>
2026-06-14 15:45:42 +0300

    add functions get_notebook_files() and delete_document_from_notebook() and tested

--- src/ingestion.py
+++ src/ingestion.py
@@ -2,8 +2,10 @@
 get_embedding_model(): loads the local embedding model.
 get_vector_store(notebook_id: int, embedding_model=None): retrieves or create vector collection for the notebook
 clear_notebook_vector_store(notebook_id: int): clears document vector for a notebook if deleted
-def load_and_split_document(file_path: str): read files, extract text, and split it into chunks
+load_and_split_document(file_path: str): read files, extract text, and split it into chunks
 add_document_to_notebook(notebook_id: int, file_path: str): processes a file and saves its vectors to the notebook's database
+get_notebook_files(notebook_id: int): scan the database and returns a list of unique file paths stored in the notebook
+delete_document_from_notebook(notebook_id: int, file_path: str): finds all document chunks from a specific file path and delete them from vector database
 """
 
 import os
@@ -85,10 +87,49 @@ def add_document_to_notebook(notebook_id: int, file_path: str):
     db.add_documents(chunks)
     print(f"Successfully added {len(chunks)} chunks to Notebook {notebook_id}.")
 
+def get_notebook_files(notebook_id: int):
+    """scan the database and returns a list of unique file paths stored in the notebook"""
+    db = get_vector_store(notebook_id)
+
+    results = db.get(include=["metadatas"])
+
+    unique_files = set()
+
+    for metadata in results.get("metadatas", []):
+        if metadata and "source" in metadata:
+            unique_files.add(metadata['source'])
+    return list(unique_files)
+
+def delete_document_from_notebook(notebook_id: int, file_path: str):
+    """finds all document chunks from a specific file path and delete them from vector database"""
+    db = get_vector_store(notebook_id)
+
+    try:
+        db._collection.delete(where={"source": file_path})
+        print(f"successfully deleted vectors for: {os.path.basename(file_path)}")
+    except Exception as e:
+        print(f"Error deleting document: {e}")
+
 
 if __name__ == "__main__":
     print("testing ...")
 
     test_file = r"D:\GitHub\LocalBook-AI\test.txt"
-    add_document_to_notebook(999, test_file)
+    test_notebook = 999
+
+    add_document_to_notebook(test_notebook, test_file)
+    print("finding files in the current notebook 999")
+    files = get_notebook_files(test_notebook)
+    for f in files:
+        print(os.path.basename(f))
+
+    print("delete the file")
+    delete_document_from_notebook(test_notebook, test_file)
+
+    print("files in notebook 999 after deletion")
+    files_after = get_notebook_files(test_notebook)
+    if not files_after:
+        print("notebook is completely empty")
+    else:
+        print("files still remain: ", files_after)
 
