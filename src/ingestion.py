097b0e99546d377b7267c1313bb1d34fc235402c
Murtada Ahmed <murtadhabg@gmail.com>
2026-06-14 15:32:27 +0300

    added functions load_and_split_document() and add_document_to_notebook() with test

--- src/ingestion.py
+++ src/ingestion.py
@@ -2,6 +2,8 @@
 get_embedding_model(): loads the local embedding model.
 get_vector_store(notebook_id: int, embedding_model=None): retrieves or create vector collection for the notebook
 clear_notebook_vector_store(notebook_id: int): clears document vector for a notebook if deleted
+def load_and_split_document(file_path: str): read files, extract text, and split it into chunks
+add_document_to_notebook(notebook_id: int, file_path: str): processes a file and saves its vectors to the notebook's database
 """
 
 import os
@@ -9,6 +11,8 @@ import shutil
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from chromadb.config import Settings
+from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 os.environ["ANONYMIZED_TELEMETRY"] = "False"
 
@@ -49,12 +53,42 @@ def clear_notebook_vector_store(notebook_id: int):
     except Exception as e:
         print(f"Error cleaning up vector collection for Notebook {notebook_id}: {e}")
 
+def load_and_split_document(file_path: str):
+    """read files, extract text, and split it into chunks"""
+    file_extension = os.path.splitext(file_path)[1].lower()
+
+    if file_extension == ".pdf":
+        loader = PyPDFLoader(file_path)
+    elif file_extension in ['.txt', '.md', '.csv']:
+        loader = TextLoader(file_path, encoding='utf-8')
+    elif file_extension == '.docx':
+        loader = Docx2txtLoader(file_path)
+    else:
+        raise ValueError(f"Unsupported file type: {file_extension}")
+
+    documents = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+    chunks = text_splitter.split_documents(documents)
+
+    return chunks
+
+def add_document_to_notebook(notebook_id: int, file_path: str):
+    """processes a file and saves its vectors to the notebook's database"""
+    print(f"Processing file: {os.path.basename(file_path)}...")
+    chunks = load_and_split_document(file_path)
+
+    db = get_vector_store(notebook_id)
+    db.add_documents(chunks)
+    print(f"Successfully added {len(chunks)} chunks to Notebook {notebook_id}.")
+
+
 if __name__ == "__main__":
     print("testing ...")
 
-    test_id = 999
-    model = get_embedding_model()
+    test_file = r"D:\GitHub\LocalBook-AI\test.txt"
+    add_document_to_notebook(999, test_file)
 
-    db = get_vector_store(test_id, model)
-
-    print(f"successfully generated collection {db._collection.name}")
